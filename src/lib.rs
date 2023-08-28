e0570c3df479cca72948ed672d6cdf6c316e4d1c
velvitoff <velvitoff@gmail.com>
2023-08-28 14:46:22 +0300

    switch to builder pattern

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,56 @@
 use image::{DynamicImage, GenericImageView};
 
-pub fn generate_text(image: &DynamicImage, include_alpha: bool) -> String {
-    let mut result = String::new();
-    let density_chars: [char; 9] = ['.',',',':','+','*','?','%','#','@'];
-    let gray_image = image.to_luma_alpha8();
-
-    for (x, y, pixel) in gray_image.enumerate_pixels() {
-        //alpha can't be zero
-        let alpha: f32 = if include_alpha {
-            u8::MAX as f32 / pixel.0[1] as f32
-        } else {
-            1.0
-        };
-        let index: usize = ((pixel.0[0] as f32 / alpha)
-            * (8 as f32 //8 is for density_chars.len()-1
-                / u8::MAX as f32))
-            .round() as usize;
-        result.push(density_chars[index]);
-
-        if (y + 1) != image.dimensions().1 && (x + 1) % image.dimensions().0 == 0 {
-            result.push('\n');
-        }
-    }
-    return result;
+#[derive(Debug)]
+pub struct TextGenerator<'a> {
+    image: &'a DynamicImage,
+    include_alpha: bool,
+    density_chars: &'a [char],
 }
 
-pub fn generate_text_with_density_chars(image: &DynamicImage, include_alpha: bool, density_chars: &Vec<char>) -> Option<String> {
-    if density_chars.len() == 0 || density_chars.len() > 256 {
-        return None;
-    }
-
-    let mut result = String::new();
-    let gray_image = image.to_luma_alpha8();
-
-    for (x, y, pixel) in gray_image.enumerate_pixels() {
-        let alpha: f32 = if include_alpha {
-            u8::MAX as f32 / pixel.0[1] as f32
-        } else {
-            1.0
-        };
-        let index: usize = ((pixel.0[0] as f32 / alpha)
-            * (density_chars.len().saturating_sub(1) as f32
-                / u8::MAX as f32))
-            .round() as usize;
-        result.push(*density_chars.get(index)?);
-
-        if (y + 1) != image.dimensions().1 && (x + 1) % image.dimensions().0 == 0 {
-            result.push('\n');
+impl<'a> TextGenerator<'a> {
+    pub fn new(image: &'a DynamicImage) -> Self {
+        TextGenerator {
+            image,
+            include_alpha: true,
+            density_chars: &['.', ',', ':', '+', '*', '?', '%', '#', '@'],
         }
     }
-    return Some(result);
+
+    pub fn set_include_alpha(&'a mut self, value: bool) -> &'a Self {
+        self.include_alpha = value;
+        self
+    }
+
+    pub fn set_density_chars(&'a mut self, value: &'a [char]) -> Option<&'a Self> {
+        if value.len() == 0 || value.len() > 256 {
+            return None;
+        }
+        self.density_chars = value;
+        Some(self)
+    }
+
+    pub fn generate(&'a self) -> String {
+        let mut result = String::new();
+        let gray_image = self.image.to_luma_alpha8();
+
+        for (x, y, pixel) in gray_image.enumerate_pixels() {
+            let alpha: f32 = if self.include_alpha {
+                u8::MAX as f32 / pixel.0[1] as f32
+            } else {
+                1.0
+            };
+            let index: usize = ((pixel.0[0] as f32 / alpha)
+                * (self.density_chars.len().saturating_sub(1) as f32 / u8::MAX as f32))
+                .round() as usize;
+            result.push(self.density_chars[index]);
+
+            if (y + 1) != self.image.dimensions().1 && (x + 1) % self.image.dimensions().0 == 0 {
+                result.push('\n');
+            }
+        }
+        result
+    }
 }
+
+
+
--- tests/gen_text.rs
+++ tests/gen_text.rs
@@ -1,5 +1,5 @@
 use image::io::Reader as ImageReader;
-use image_ascii::{generate_text, generate_text_with_density_chars};
+use image_ascii::TextGenerator;
 
 //use std::io::Write as _;
 //let mut file = std::fs::File::create("hek.txt").unwrap();
@@ -7,17 +7,18 @@ use image_ascii::{generate_text, generate_text_with_density_chars};
 
 #[test]
 fn gen_text_diamond_sword() {
-    let file =
-        std::fs::read_to_string("tests/data/results_text/diamond_sword.txt").unwrap();
+    let file = std::fs::read_to_string("tests/data/results_text/diamond_sword.txt").unwrap();
     let image = ImageReader::open("tests/data/images/diamond_sword.png")
         .unwrap()
         .decode()
         .unwrap();
 
-    let res = generate_text(&image, true);
+    let res = TextGenerator::new(&image).generate();
     assert_eq!(res, file);
 
-    let res = generate_text(&image, false);
+    let res = TextGenerator::new(&image)
+        .set_include_alpha(false)
+        .generate();
     assert_eq!(res, file);
 }
 
@@ -29,10 +30,12 @@ fn gen_text_rust_icon() {
         .decode()
         .unwrap();
 
-    let res = generate_text(&image, true);
+    let res = TextGenerator::new(&image).generate();
     assert_eq!(res, file);
 
-    let res = generate_text(&image, false);
+    let res = TextGenerator::new(&image)
+        .set_include_alpha(false)
+        .generate();
     assert_eq!(res, file);
 }
 
@@ -44,59 +47,57 @@ fn gen_text_cactus() {
         .decode()
         .unwrap();
 
-    let res = generate_text(&image, true);
+    let res = TextGenerator::new(&image).generate();
     assert_eq!(res, file);
 
-    let res = generate_text(&image, false);
+    let res = TextGenerator::new(&image)
+        .set_include_alpha(false)
+        .generate();
     assert_eq!(res, file);
 }
 
 #[test]
 fn gen_text_every_blackwhite() {
-    let file1 =
-        std::fs::read_to_string("tests/data/results_text/every_blackwhite.txt").unwrap();
-    let file2 = std::fs::read_to_string(
-        "tests/data/results_text/every_blackwhite_double_density.txt",
-    )
-    .unwrap();
+    let file1 = std::fs::read_to_string("tests/data/results_text/every_blackwhite.txt").unwrap();
+    let file2 =
+        std::fs::read_to_string("tests/data/results_text/every_blackwhite_double_density.txt")
+            .unwrap();
     let image = ImageReader::open("tests/data/images/every_blackwhite.png")
         .unwrap()
         .decode()
         .unwrap();
 
-    let res = generate_text(&image, true);
+    let res = TextGenerator::new(&image).generate();
     assert_eq!(res, file1);
 
-    let res = generate_text_with_density_chars(
-        &image,
-        true,
-        &vec![
+    let res = TextGenerator::new(&image)
+        .set_density_chars(&vec![
             '.', ',', ':', '+', '*', '?', '%', '#', '@', '.', ',', ':', '+', '*', '?', '%', '#',
             '@',
-        ],
-    )
-    .unwrap();
+        ])
+        .unwrap()
+        .generate();
     assert_eq!(res, file2);
 }
 
 #[test]
 fn gen_text_something_transparent() {
     let file =
-        std::fs::read_to_string("tests/data/results_text/transparent_something.txt")
+        std::fs::read_to_string("tests/data/results_text/transparent_something.txt").unwrap();
+    let file_no_alpha =
+        std::fs::read_to_string("tests/data/results_text/transparent_something_no_alpha.txt")
             .unwrap();
-    let file_no_alpha = std::fs::read_to_string(
-        "tests/data/results_text/transparent_something_no_alpha.txt",
-    )
-    .unwrap();
     let image = ImageReader::open("tests/data/images/transparent_something.png")
         .unwrap()
         .decode()
         .unwrap();
 
-    let res = generate_text(&image, true);
+    let res = TextGenerator::new(&image).generate();
     assert_eq!(res, file);
 
-    let res = generate_text(&image, false);
+    let res = TextGenerator::new(&image)
+        .set_include_alpha(false)
+        .generate();
     assert_eq!(res, file_no_alpha);
 }
 
@@ -107,15 +108,15 @@ fn gen_text_density_chars_len0() {
         .decode()
         .unwrap();
 
-    let res = generate_text_with_density_chars(&image, true, &vec![]);
-    assert_eq!(res, None);
+    let value = TextGenerator::new(&image)
+        .set_density_chars(&vec![])
+        .is_none();
+    assert_eq!(value, true);
 }
 
 #[test]
 fn gen_text_density_chars_len256() {
-    let file =
-        std::fs::read_to_string("tests/data/results_text/every_blackwhite_256.txt")
-            .unwrap();
+    let file = std::fs::read_to_string("tests/data/results_text/every_blackwhite_256.txt").unwrap();
     let image = ImageReader::open("tests/data/images/every_blackwhite.png")
         .unwrap()
         .decode()
@@ -140,7 +141,10 @@ fn gen_text_density_chars_len256() {
     ];
     assert_eq!(chars_256.len(), 256);
 
-    let res = generate_text_with_density_chars(&image, true, &chars_256).unwrap();
+    let res = TextGenerator::new(&image)
+        .set_density_chars(&chars_256)
+        .unwrap()
+        .generate();
     assert_eq!(res, file);
 }
 
@@ -152,7 +156,6 @@ fn ascii_image_density_chars_len_too_large() {
         .unwrap();
 
     let chars_257: Vec<char> = Vec::from_iter((0..257).map(|_| '1'));
-
-    let res = generate_text_with_density_chars(&image, true, &chars_257);
-    assert_eq!(res, None);
+    let value = TextGenerator::new(&image).set_density_chars(&chars_257).is_none();
+    assert_eq!(value, true);
 }
